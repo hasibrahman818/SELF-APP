683bbde10e13b49003b5d0f05530586fa145de3d
Hasib Rahman <hasibrahman818@gmail.com>
2025-04-27 22:02:21 +0100

    Changed again!

---
+++ app/index.jsx
@@ -0,0 +1,11 @@
+import {Stylesheet, Text, View}  from 'react-native'
+const Home = () =>{
+    return(
+        <View>
+            <Text>Home</Text>
+        </View>
+    )
+}
+
+export default Home
+const styles = Stylesheet.create({})
